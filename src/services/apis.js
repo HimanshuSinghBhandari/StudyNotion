4939391a78c76ea8e5660c31f51a7f766a83276d
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-01-23 19:20:58 +0530

    first commit

---
+++ src/services/apis.js
@@ -0,0 +1,14 @@
+const BASE_URL = "https://localhost:4000/api/v1"
+
+export const categories = {
+  CATEGORIES_API: BASE_URL + "/course/showAllCategories",
+
+};
+
+export const contactusEndpoint = {
+  CONTACT_US_API: BASE_URL + "/reach/contact",
+}
+
+export const ratingsEndpoints = {
+  REVIEWS_DETAILS_API: BASE_URL + "/course/getReviews",
+}
